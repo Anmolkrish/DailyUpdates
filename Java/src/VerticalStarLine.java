1905ff3ce710430c92c031bc3a91cfde4c5ec613
Anmol C Satakhed <anmolkrish74@gmail.com>
2026-06-19 10:39:13 +0530

    Added Vertical Star Line

---
+++ Java/src/VerticalStarLine.java
@@ -0,0 +1,14 @@
+public class VerticalStarLine {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++) {
+
+            if (i <= 5) {
+                System.out.println("*");
+            } else {
+                System.out.println();
+            }
+
+        }
+    }
+}
